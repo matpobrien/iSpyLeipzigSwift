eacf52c1845dd5ae9d5bc7b24e403d27f9de7b08
Matthew Paul O'Brien <matpobrien@gmail.com>
2021-05-23 11:38:54 +0200

    Initial Commit

---
+++ iSpyLeipzig/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iSpyLeipzig
+//
+//  Created by Mat on 23.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iSpyLeipzig/iSpyLeipzigApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSpyLeipzigApp.swift
+//  iSpyLeipzig
+//
+//  Created by Mat on 23.05.21.
+//
+
+import SwiftUI
+
+@main
+struct iSpyLeipzigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
